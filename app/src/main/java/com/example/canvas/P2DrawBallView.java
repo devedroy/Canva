a339bfd563bfcb33395c0caead7a11ffe182424a
Roy Devpreyo <devedroy.dr@gmail.com>
2022-01-24 13:19:11 +0530

    Two viewer changes

--- app/src/main/java/com/example/canvas/P2DrawBallView.java
+++ app/src/main/java/com/example/canvas/P2DrawBallView.java
@@ -1,2 +1,49 @@
-package com.example.canvas;public class P2DrawBallView {
+package com.example.canvas;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.view.View;
+
+public class P2DrawBallView extends View {
+
+    private float currX;
+    private float currY;
+
+    public P2DrawBallView(Context context) {
+        super(context);
+    }
+
+    public float getCurrX() {
+        return currX;
+    }
+
+    public float getCurrY() {
+        return currY;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        Paint paint = new Paint();
+        paint.setColor(this.getBallColor());
+
+        canvas.drawCircle(currX, currY, 35, paint);
+    }
+
+    private int getBallColor() {
+    }
+
+    public void setCurrX(float currX) {
+        this.currX = currX;
+    }
+
+    public void setCurrY(float currY) {
+        this.currY = currY;
+    }
+
+    public void setBallColor(int blue) {
+
+    }
 }
--- app/src/main/java/com/example/canvas/P2RotateBallCustomView.java
+++ app/src/main/java/com/example/canvas/P2RotateBallCustomView.java
@@ -2,7 +2,11 @@ package com.example.canvas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class P2RotateBallCustomView extends AppCompatActivity {
 
@@ -10,5 +14,29 @@ public class P2RotateBallCustomView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_p2_rotate_ball_custom_view);
+
+        LinearLayout rootLayout = (LinearLayout) findViewById(R.id.idP2DrawBallView);
+
+        final P2DrawBallView drawBallView = new P2DrawBallView(this);
+
+        drawBallView.setMinimumWidth(500);
+        drawBallView.setMinimumHeight(800);
+
+        View.OnTouchListener onTouchListener = new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                drawBallView.setCurrX(event.getX());
+                drawBallView.setCurrY(event.getY());
+
+                drawBallView.setBallColor(Color.BLUE);
+
+                drawBallView.invalidate();
+
+                return true;
+            }
+        };
+
+        drawBallView.setOnTouchListener(onTouchListener);
+        rootLayout.addView(drawBallView);
     }
 }
